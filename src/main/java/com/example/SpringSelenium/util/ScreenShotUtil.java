904a563afb5e918f18341b4129933058439f51a8
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2022-05-24 15:15:33 +0200

    Parallel testing using testng

--- src/main/java/com/example/SpringSelenium/util/ScreenShotUtil.java
+++ src/main/java/com/example/SpringSelenium/util/ScreenShotUtil.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
@@ -18,7 +19,7 @@ import java.nio.file.Path;
 @Component
 public class ScreenShotUtil {
     @Autowired
-    private TakesScreenshot driver;
+    private ApplicationContext ctx;
 
     @Value("${screenshot.path}")
     private Path path;
@@ -34,12 +35,11 @@ public class ScreenShotUtil {
             } catch (InterruptedException ie) {
                 ie.printStackTrace();
             }
-            System.out.println("Sleeping...");
         }
     }
 
     public void takeScreenShot() throws IOException {
-        File sourceFile = this.driver.getScreenshotAs(OutputType.FILE);
+        File sourceFile = this.ctx.getBean(TakesScreenshot.class).getScreenshotAs(OutputType.FILE);
         FileCopyUtils.copy(sourceFile, this.path.resolve(faker.name().firstName()+".png").toFile());
     }
 }
